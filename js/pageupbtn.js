5a04f8b0b2eca2ab3949060f5dd4e7b044956afa
Dhanuka Perera <uldsperera2021@gmail.com>
2022-11-02 17:21:04 +0530

    Website v1.0
    In this version of the site below mentioned improvements and features implemented.
    1. Improved accessibility (alt, type, readability, etc.)
    2. Improved performance (removed unnecessary code parts)
    3. Partially improved SEO (added meta tags)
    4. Improved responsiveness
    5. Few features added (Page Up Button, pre-loader, page transition animations)

---
+++ js/pageupbtn.js
@@ -0,0 +1,15 @@
+// let mybutton = document.getElementById("pageUpBtn");
+// window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//     if (document.body.scrollTop > 180 || document.documentElement.scrollTop > 180) {
+//         mybutton.style.display = "block";
+//     } else {
+//         mybutton.style.display = "none";
+//     }
+// }
+// function topFunction() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// }
+
+// No Need of this code as it is in main files.
